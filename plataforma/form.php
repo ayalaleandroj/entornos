1a6dfe38e5fd0e25a258137e71bb20f7d0d72605
Osvaldo-PC\Osvaldo <osvaldovallone@outlook.com>
2018-06-15 15:38:06 -0300

    prueba.

--- plataforma/form.php
+++ plataforma/form.php
@@ -1,4 +1,5 @@
 ﻿<!DOCTYPE html>
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
@@ -7,7 +8,7 @@
     <meta name="author" content="" />
     <!--[if IE]>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
+        <![endif] lklkllklk-->
     <title>ABM de Usuarios</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
